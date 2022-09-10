28b3679037ba9bd928173ae06b61b23b7e257add
Fábio Paredes <f.paredes.o@gmail.com>
2022-09-10 12:02:16 -0300

    Criando o footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,45 @@
 import { memo } from 'react'
 
+import { Container } from 'react-bootstrap'
+import { BsFacebook, BsInstagram, BsTwitter, BsYoutube } from 'react-icons/bs'
+
+import MaricaRecebe from 'assets/MaricaRecebe.png'
+import MaricaTurismo from 'assets/MaricaTurismo.png'
+
+import { FooterContainer, ImgMaricaRecebe } from './style'
+
 const Footer: React.FC = () => {
   return (
-    <footer className="d-flex">
-      <div>teste1</div>
-      <div>teste2</div>
-    </footer>
+    <FooterContainer className="py-4">
+      <Container className="d-flex flex-column flex-xl-row justify-content-between">
+        <div>
+          <div className="d-flex align-items-center justify-content-center">
+            <BsFacebook color="fff" size={18} className="me-2" />
+            <p className="d-none d-sm-flex me-3 text-light">Facebook</p>
+            <BsInstagram color="fff" size={18} className="me-2" />
+            <p className="d-none d-sm-flex me-3 text-light">Instagram</p>
+            <BsTwitter color="fff" size={18} className="me-2" />
+            <p className="d-none d-sm-flex me-3 text-light">Twitter</p>
+            <BsYoutube color="fff" size={18} className="me-2" />
+            <p className="d-none d-sm-flex me-3 text-light">Youtube</p>
+          </div>
+          <p className="text-center mt-3 text-light h5">Área do Comerciante</p>
+        </div>
+
+        <div className="d-flex flex-column flex-md-row justify-content-center align-items-center">
+          <ImgMaricaRecebe
+            src={MaricaRecebe}
+            className="img-fluid"
+            alt="Maricá Recebe"
+          />
+          <div className="mx-4">
+            <p className="text-center mt-3 text-light h5">Manual Gastronomia</p>
+            <p className="text-center mt-3 text-light h5">Manual Hospedagem</p>
+          </div>
+          <img src={MaricaTurismo} className="img-fluid" alt="Maricá Turismo" />
+        </div>
+      </Container>
+    </FooterContainer>
   )
 }
 
---
+++ src/components/Footer/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const FooterContainer = styled.footer`
+  background-color: #333;
+`
+
+export const ImgMaricaRecebe = styled.img`
+  max-width: 150px;
+`
